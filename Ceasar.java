f664e996b516bf47bd9c17021c2a0b0290e0d3db
Minh Quang <151762671+lmQuanGGGG@users.noreply.github.com>
2024-11-28 16:58:39 +0700

    Add files via upload

---
+++ Ceasar.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package a.ceasar;
+
+/**
+ *
+ * @author Asus
+ */
+public class Ceasar {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Ceasarr a = new Ceasarr();
+        a.setVisible(true);
+    }
+}
